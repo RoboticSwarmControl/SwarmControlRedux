4b69ae1369d63fc38269f254ecbb9b405f51604c
Chris Ertel <chris@kedagital.com>
2016-07-09 01:13:46 -0500

    Add fetching routes for result data.

--- server/resultController.js
+++ server/resultController.js
@@ -1,17 +1,70 @@
 'use strict';
 function URFP( x ) { /* jshint expr:true */ x; }
 var express = require('express');
-
 var router = express.Router();
+
+var db = require('./database.js');
+var csv = require('fast-csv');
 
 router.get('/', function _renderResultsIndex( req, res ) {
 	URFP(req);
-	res.status(200).send('Results!');
+	db.getResults()
+	.then( function( results ) {
+		// helpful trick from http://expressjs.com/en/4x/api.html#res.format
+		res.format({			
+			'text/csv': function() {
+				csv.writeToString(	results,
+									{ headers: true},
+									function (err, data) {
+											if (err ){
+												res.status(500).send( err.toString() );
+											} else {
+												res.status(200).send( data );
+											}
+									});
+			},
+			'text/json': function() {
+				res.status(200).json( {res:results} );
+			},
+			'default': function () {
+				res.sendStatus(406); // bad MIME type requested
+			}
+		});		
+	})
+	.catch( function ( err ) {
+		console.log( err.toString() );
+		res.status(500).send( err.toString() );
+	});
 });
 
-router.get('/:resultID', function _renderResultsIndex( req, res ) {
-	URFP(req);
-	res.status(200).send('Result '+ req.params.resultID);
+router.get('/:resultID', function _renderResultsIndex( req, res ) {	
+	db.getResultsForTask( req.params.resultID)
+	.then( function( results ) {
+		// helpful trick from http://expressjs.com/en/4x/api.html#res.format
+		res.format({			
+			'text/csv': function() {
+				csv.writeToString(	results,
+									{ headers: true},
+									function (err, data) {
+											if (err ){
+												res.status(500).send( err.toString() );
+											} else {
+												res.status(200).send( data );
+											}
+									});
+			},
+			'text/json': function() {
+				res.status(200).json( {res:results} );
+			},
+			'default': function () {
+				res.sendStatus(406); // bad MIME type requested
+			}
+		});		
+	})
+	.catch( function ( err ) {
+		console.log( err.toString() );
+		res.status(500).send( err.toString() );
+	});
 });
 
 router.post('/', function _renderResultsIndex( req, res ) {
--- server/server.js
+++ server/server.js
@@ -4,13 +4,20 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var db = require('./database.js');
 var gameRouter = require('./gameController.js');
 var resultRouter = require('./resultController.js');
 var appRouter = require('./appController.js');
 
+var listenPort = process.env.LISTEN_PORT || 3000;
+
+console.log('Listening on port ', listenPort);
+console.log('Connecting to database ', process.env.DB_STRING);
+
+db.init( process.env.DB_STRING);
 app.use('/results', resultRouter);
 app.use('/games', gameRouter);
 app.use('/', appRouter);
 app.use('/assets', express.static( path.join(__dirname, '..', 'dist') ));
 
-app.listen(3000);
+app.listen( listenPort );
